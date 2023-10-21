2d157eecec464a7d2fdf2133464e49b18061286a
iym666 <example@gmail.com>
2023-10-22 00:17:07 +0500

    practiced with math of loops with new Date()

--- work with dates/script.js
+++ work with dates/script.js
@@ -1,9 +1,11 @@
 'use strict';
 
-const now = new Date();
+const now = new Date('2023-10-22');
+  // new Date.parse('2023-10-22')
 
 //setters
-
+console.log(now.setHours(40));
+console.log(now)
 
 // getters
 // console.log(now.getFullYear());
@@ -14,3 +16,13 @@ const now = new Date();
 
 // console.log(now.getTimezoneOffset());
 // console.log(now.getTime());
+
+let start = new Date();
+
+for (let i = 0; i < 100000; i++) {
+  let some = i ** 3;
+}
+
+let end = new Date();
+
+alert(`Loop worked for ${end - start} millsec`); // 4
