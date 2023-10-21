10cd95c164fc005726830ea673135a7d003782f0
iym666 <example@gmail.com>
2023-10-22 00:10:29 +0500

    used all getters

--- work with dates/script.js
+++ work with dates/script.js
@@ -2,4 +2,15 @@
 
 const now = new Date();
 
-console.log(now);
+//setters
+
+
+// getters
+// console.log(now.getFullYear());
+// console.log(now.getMonth());
+// console.log(now.getDate());
+// console.log(now.getHours());
+// console.log(now.getUTCHours());
+
+// console.log(now.getTimezoneOffset());
+// console.log(now.getTime());
