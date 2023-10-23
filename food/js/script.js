3b8c2fcf1f4de724d74843527699e7b57f8a1c14
iym666 <example@gmail.com>
2023-10-24 00:50:54 +0500

    refactored code and created new function closeModal to avoid DRY in script and added event to close modal window with Esc button on keyboard

--- food/js/script.js
+++ food/js/script.js
@@ -95,21 +95,37 @@ window.addEventListener('DOMContentLoaded', () => {
 
   // Modal
 
-  const modalTrigger = document.querySelector('[data-modal]'),
+  const modalTrigger = document.querySelectorAll('[data-modal]'),
         modal = document.querySelector('.modal'),
         modalCloseBtn = document.querySelector('[data-close]');
 
-  modalTrigger.addEventListener('click', () => {
-    modal.classList.add('show');
-    modal.classList.remove('hide');
-    // modal.classList.toggle('show');
-    document.body.style.overflow = 'hidden';
+  modalTrigger.forEach(btn => {
+    btn.addEventListener('click', () => {
+      modal.classList.add('show');
+      modal.classList.remove('hide');
+      // modal.classList.toggle('show');
+      document.body.style.overflow = 'hidden';
+    });
   });
 
-  modalCloseBtn.addEventListener('click', () => {
+  function closeModal() {
     modal.classList.add('hide');
     modal.classList.remove('show');
     // modal.classList.toggle('show');
     document.body.style.overflow = '';
+  }
+
+  modalCloseBtn.addEventListener('click', closeModal);
+
+  modal.addEventListener('click', (e) => {
+    if (e.target === modal) {
+      closeModal();
+    }
+  });
+
+  document.addEventListener('keydown', (e) => {
+    if (e.code === 'Escape' && modal.classList.contains('show')) {
+      closeModal();
+    }
   });
 });
